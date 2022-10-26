78d60447ef56e72e7a043745cd61d54e49ca63cf
MrBorges <lucas.info@gmail.com>
2022-10-26 17:58:35 -0300

    Added Rank component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
 
 function App() {
   return (
     <div className="App">
+
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
+      <ParticlesBg type="circle" bg={true} />
+
       {/*<FaceRecognition />*/}
     </div>
   );
